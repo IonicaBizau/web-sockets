4a185038af4668b73d8bb8da006a70c5bcfc77ef
Ionică Bizău <bizauionica@yahoo.com>
2013-10-24 18:51:54 +0300

    Comments.

--- operations.js
+++ operations.js
@@ -25,26 +25,74 @@ exports.emit = function (link) {
 };
 
 
+/*
+ *  init ()
+ *  This function set the io variable
+ *
+ * */
 function init () {
     // Socket.io server listens to our app
     io = SocketIO.listen(M.app.server);
 }
-M.on("sockets.init", init);
 
+/*
+ *  emit (object, function)
+ *  Emit some event and data
+ *
+ *  options:
+ *   - event: the event name
+ *   - data:  data that must be emited
+ *
+ *  callback:
+ *   - callback function
+ * */
 function emit (options, callback) {
+
+    // options must be an object
     options = options || {};
+
+    // callback must be a function
     callback = callback || function () {};
+
+    // init io, if it is not inited
     if (!io) { init(); }
+
+    // emit that event and data
     io.sockets.emit(options.event, options.data);
 }
-M.on("sockets.emit", emit);
 
+/*
+ *  listen (object, function)
+ *  Listen on some event and callback the socket
+ *
+ *  options:
+ *   - event: the event name that listen to
+ *
+ *  callback: the function that must be fired
+ *            on that event
+ *
+ * */
 function listen (options, callback) {
+
+    // options must be an object
     options = options || {};
+
+    // callback must be a function
     callback = callback || function () {};
+
+    // init io, if it is not inited
     if (!io) { init(); }
-    io.sockets.on(options.event, function (socket) {
-        callback(socket);
-    });
+
+    // listen on that event
+    io.sockets.on(options.event, callback);
 }
+
+/*
+ *  The events interface:
+ *   - sockets.init
+ *   - sockets.emit
+ *   - sockets.listen
+ * */
+M.on("sockets.init", init);
+M.on("sockets.emit", emit);
 M.on("sockets.listen", listen);
