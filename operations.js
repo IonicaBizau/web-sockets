0397ea50ad7af1986c88ac44dab8bfdf17f91616
Ionică Bizău <bizauionica@yahoo.com>
2013-10-24 18:45:20 +0300

    Added module functions.

--- operations.js
+++ operations.js
@@ -1,10 +1,11 @@
+var SocketIO = require("socket.io");
+var io;
+
 /*
  *  Init
  * */
 exports.init = function (link) {
-    // console.log(M.app.server);
-    // TODO
-    link.send(200, { op: "init", data: link.data });
+    init();
 };
 
 /*
@@ -22,3 +23,28 @@ exports.emit = function (link) {
     // TODO
     link.send(200, { op: "emit" });
 };
+
+
+function init () {
+    // Socket.io server listens to our app
+    io = SocketIO.listen(M.app.server);
+}
+M.on("sockets.init", init);
+
+function emit (options, callback) {
+    options = options || {};
+    callback = callback || function () {};
+    if (!io) { init(); }
+    io.sockets.emit(options.event, options.data);
+}
+M.on("sockets.emit", emit);
+
+function listen (options, callback) {
+    options = options || {};
+    callback = callback || function () {};
+    if (!io) { init(); }
+    io.sockets.on(options.event, function (socket) {
+        callback(socket);
+    });
+}
+M.on("sockets.listen", listen);
